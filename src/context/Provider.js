7d0ef32124943ca1a99abf6ee5dc31cc304fdaca
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-09-16 06:10:05 -0300

    building provider

---
+++ src/context/Provider.js
@@ -0,0 +1,30 @@
+// O Provider é um componente de classe
+import React from "react";
+
+
+class Provider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+      signal: { color: 'red' },
+    }
+  }
+
+  moveCar = (car, side) =>  {
+    this.setState({
+      ...this.state,
+      cars[car]:side,
+    })
+  }
+
+  render() {
+    return (null)
+  }
+}
+
+export default Provider;
