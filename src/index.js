0511b122e9c8c2a375f9b7222eb339e962c8cc5d
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-09-16 06:27:14 -0300

    setup provider into index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux';
+// import { Provider } from 'react-redux';
+// import store from './redux';
+import Provider from './context/Provider';
+import carContext from './context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider value={carContext}>
       <App />
     </Provider>
   </React.StrictMode>,
