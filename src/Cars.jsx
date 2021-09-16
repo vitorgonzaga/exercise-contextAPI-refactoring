84b3584ffc9642a82864bf636926db6203e91bad
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-09-16 06:31:58 -0300

    working on Cars component

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,26 +1,29 @@
 // src/Cars.jsx
 
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
-import { moveCar } from './redux/actionCreators';
+// import { moveCar } from './redux/actionCreators';
 
-function Cars({ redCar, blueCar, yellowCar, moveCar }) {
-  return (
-    <div>
+class Cars extends React.Component {
+  render(){
+    const { red: redCar, blue: blueCar, yellow: yellowCar } = this.context.cars;
+    const { moveCar } = this.context;
+    return (
+      <div>
       <div>
         <img
           className={redCar ? 'car-right' : 'car-left'}
           src={carRed}
           alt="red car"
-        />
+          />
         <button
           onClick={() => moveCar('red', !redCar)}
           type="button"
-        >
+          >
           Move
         </button>
       </div>
@@ -29,11 +32,11 @@ function Cars({ redCar, blueCar, yellowCar, moveCar }) {
           className={blueCar ? 'car-right' : 'car-left'}
           src={carBlue}
           alt="blue car"
-        />
+          />
         <button
           onClick={() => moveCar('blue', !blueCar)}
           type="button"
-        >
+          >
           Move
         </button>
       </div>
@@ -42,30 +45,35 @@ function Cars({ redCar, blueCar, yellowCar, moveCar }) {
           className={yellowCar ? 'car-right' : 'car-left'}
           src={carYellow}
           alt="yellow car"
-        />
+          />
         <button
           onClick={() => moveCar('yellow', !yellowCar)}
           type="button"
-        >
+          >
           Move
         </button>
       </div>
     </div>
   );
 }
+}
 
-Cars.propTypes = {
-  moveCar: PropTypes.func.isRequired,
-  blueCar: PropTypes.bool.isRequired,
-  redCar: PropTypes.bool.isRequired,
-  yellowCar: PropTypes.bool.isRequired,
-};
+// Cars.propTypes = {
+//   moveCar: PropTypes.func.isRequired,
+//   blueCar: PropTypes.bool.isRequired,
+//   redCar: PropTypes.bool.isRequired,
+//   yellowCar: PropTypes.bool.isRequired,
+// };
 
-const mapStateToProps = (state) => ({
-  redCar: state.carReducer.cars.red,
-  blueCar: state.carReducer.cars.blue,
-  yellowCar: state.carReducer.cars.yellow});
+// const mapStateToProps = (state) => ({
+//   redCar: state.carReducer.cars.red,
+//   blueCar: state.carReducer.cars.blue,
+//   yellowCar: state.carReducer.cars.yellow});
 
-const mapDispatchToProps = { moveCar };
+// const mapDispatchToProps = { moveCar };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Cars);
+// export default connect(mapStateToProps, mapDispatchToProps)(Cars);
+
+Cars.connectType = carContext;
+
+export default Cars;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import App from './App';
 // import { Provider } from 'react-redux';
 // import store from './redux';
 import Provider from './context/Provider';
-import carContext from './context';
+// import carContext from './context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider value={carContext}>
+    <Provider>
       <App />
     </Provider>
   </React.StrictMode>,
